0fcc3d8ed4518c6877217d4d2b28ea4e05cb134d
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-13 16:15:38 -0800

    NewActivity Java file converted to Kotlin File

---
+++ app/src/main/java/com/example/kotlin/NewActivity.kt
@@ -0,0 +1,23 @@
+package com.example.kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+
+/**
+ * Created by kangsik_kevin_lee on 2/13/18.
+ */
+
+class NewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+    }
+
+    fun sendMessage(view: View) {
+
+    }
+
+}
