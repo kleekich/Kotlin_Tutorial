d6a1cbe294824db8a8bb991547368d411901e364
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-13 19:08:20 -0800

    NewActivity is edited

--- app/src/main/java/com/example/kotlin/NewActivity.kt
+++ app/src/main/java/com/example/kotlin/NewActivity.kt
@@ -3,6 +3,7 @@ package com.example.kotlin
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import kotlinx.android.synthetic.main.activity_new.*
 
 
 /**
@@ -14,6 +15,8 @@ class NewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
+
+        textView.setText("Kotlin, view Binding!");
     }
 
     fun sendMessage(view: View) {
